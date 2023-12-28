d24e6d72380a09299b38192138ca9dadc0a68449
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-12-28 12:49:30 +0300

    task 2.2

---
+++ 2.2/main.js
@@ -0,0 +1,11 @@
+let firstName = 'Evgeniia';
+let lastName = 'Shirshikova';
+let age = '37'
+let city = 'Moscow'
+let hobbie = 'drawing'
+
+function sum(a,b) {
+    return a+b;
+}
+
+console.log(`Hello, I am ${firstName} ${lastName}! I'm ${age}. I live in ${city}. My hobbie is a ${hobbie}`);
