f0a9f3ae4a5b96a2a21f9ae20007d97d6ae31e34
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-12-28 13:05:47 +0300

    2.3

--- 2.2/main.js
+++ 2.2/main.js
@@ -1,11 +1,8 @@
 let firstName = 'Evgeniia';
 let lastName = 'Shirshikova';
-let age = '37'
-let city = 'Moscow'
-let hobbie = 'drawing'
+let age = 37;
+let city = 'Moscow';
+let hobbie = 'drawing';
 
-function sum(a,b) {
-    return a+b;
-}
 
 console.log(`Hello, I am ${firstName} ${lastName}! I'm ${age}. I live in ${city}. My hobbie is a ${hobbie}`);
---
+++ 2.3/main.js
@@ -0,0 +1,6 @@
+let titleProject = 'Оценка стоимости';
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+let screenPrice = 10000;
+let percentage = 10;
+let fullPrice = 100000;
+let responsive = true;
